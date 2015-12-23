dfda1a45e9526972731fb09afc8feff2e3fb9793
RobLanf <rob.lanfear@gmail.com>
2015-12-23 11:18:19 +1100

    update merge.reads documentation
    and make user output nicer

--- R/merge.reads.R
+++ R/merge.reads.R
@@ -1,12 +1,7 @@
 #' Merge fwd and reverse reads into a single sequence
 #' 
-#' This function attempts to merge any number of forward and reverse sequences into a single consensus sequence. It calculates a number of statistics that should help you decide whether a sensible consensus sequence exists for your data.
+#' This function attempts to merge any number of forward and reverse sequences into a single consensus sequence. It calculates a number of statistics that should help you decide whether a sensible consensus sequence exists for your data.\cr
 #' Note that sequence names can be provided via the names on the input lists. If this is done, the names will be preserved in the output data. Otherwise, sequences will be called: fwd_1, fwd_2, etc.
-#' 1. An alignment of the forward and reverse reads (to view, use the BrowseSeqs() function from DECIPHER), as an XStringSet object. This alignment includes the aligned consensus sequence calculated below as "consensus"
-#' 2. A consensus sequence (as an XString obejct) calculated from the alignment. This sequence ignores leading and trailing gaps in the alignment. Calculation of the consensus can be controlled with minReads and minInformation options. Note, this is not the same as the consensus you would see using BrowseSeqs() on the alignment, which is calculated differently.
-#' 3. A data frame that shows the number of mismatches of each sequence to the consensus
-#' 4. A distance matrix of all input reads to each other (ignoring all alignment gaps)
-#' 5. A dendrogram depicting the distance matrix, use plot() to view it.
 #'
 #' @param fwd.seqs a list of forward sequences, each should be a sangerseq s4 object from the sangerseqR package. These sequences will not be reverse-complemented before alignment.
 #' @param rev.seqs a list of reverse sequences, each should be a sangerseq s4 object from the sangerseqR package. These sequences will be reverse-complemented before alignment.
@@ -17,12 +12,14 @@
 #' @param genetic.code Named character vector in the same format as GENETIC_CODE (the default), which represents the standard genetic code. This is the code with which the function will attempt to translate your DNA sequences. You can get an appropriate vector with the getGeneticCode() function. The default is the standard code.
 #' 
 #' @return a list with the following components:
-#'              1. indel.df: If you supplied an amino acid reference sequence, this data frame will contain the number of insertions and deletions made to correct the frame of your reads, plus the distance of each read's best translation from the reference sequence.
-#'
-#'
-#'
-#'
-#'
+#'          \enumerate{
+#'              \item consensus: the consensus sequence from the merged reads.\cr
+#'              \item alignment: the alignment of all the reads, with the called consensus sequence. E.g. if the list was called 'merged.reads', you could use BrowseSeqs(merged.reads$alignment) to view the alignment.
+#'              \item differences: a data frame of the number of pairwise differences between each read and the consensus sequence, as well as the number of bases in each input read that did not contribute to the consensus sequence. Can assist in detecting incorrect reads, or reads with a lot of errors.
+#'              \item distance.matrix: a distance matrix of genetic distances (corrected with the JC model) between all of the input reads.
+#'              \item dendrogram: a dendrogram depicting the distance.matrix. E.g. if the list was called 'merged.reads', you could use plot(merged.reads$dendrogram) to see the dendrogram.
+#'              \item indels: if you specified a reference sequence via ref.aa.seq, then this will be a data frame describing the number of indels and deletions that were made to each of the input reads in order to correct frameshift mutations.
+#'          }
 #'
 #' @keywords merge, reads, alignment, sequence
 #'
@@ -44,6 +41,7 @@ merge.reads <- function(fwd.seqs, rev.seqs, ref.aa.seq = NULL, minInformation = 
     }
 
     # Reverse complement reverse reads, and join into DNAStringSet
+    print("Reverse compelementing reverse reads")
     fwd = mclapply(fwd.seqs, get.primaryseq.xstring, revcomp = FALSE, mc.cores = processors) 
     rev = mclapply(rev.seqs, get.primaryseq.xstring, revcomp = TRUE, mc.cores = processors) 
     seqs = DNAStringSet(c(fwd, rev))
@@ -51,7 +49,8 @@ merge.reads <- function(fwd.seqs, rev.seqs, ref.aa.seq = NULL, minInformation = 
 
     # Try to correct framshifts in the input sequences 
     if(!is.null(ref.aa.seq)) {
-        corrected = CorrectFrameshifts(myXStringSet = seqs, myAAStringSet = AAStringSet(ref.aa.seq), geneticCode = genetic.code, type = 'both', processors = processors)
+        print("Correcting frameshifts in reads using amino acid reference sequence")
+        corrected = CorrectFrameshifts(myXStringSet = seqs, myAAStringSet = AAStringSet(ref.aa.seq), geneticCode = genetic.code, type = 'both', processors = processors, verbose = FALSE)
         seqs = corrected$sequences
         indels = get.indel.df(corrected$indels)        
     }else{
@@ -60,30 +59,34 @@ merge.reads <- function(fwd.seqs, rev.seqs, ref.aa.seq = NULL, minInformation = 
 
 
     # align the sequences
+    print("Aligning reads")
     if(!is.null(ref.aa.seq)){
-        aln = AlignTranslation(seqs, geneticCode = genetic.code, processors = processors)
+        aln = AlignTranslation(seqs, geneticCode = genetic.code, processors = processors, verbose = FALSE)
     }else{
-        aln = AlignSeqs(seqs, processors = processors)
+        aln = AlignSeqs(seqs, processors = processors, verbose = FALSE)
     }
     names(aln) = c(names(fwd.seqs), names(rev.seqs))
 
     # call consensus
+    print("Calling consensus sequence")
     n.reads = length(seqs)
     implied.minInfo = minReads/n.reads
     minInformation = max(c(minInformation, implied.minInfo))
-    consensus = ConsensusSequence(aln, 
-                                  minInformation = minInformation, 
+    consensus = ConsensusSequence(aln,
+                                  minInformation = minInformation,
                                   includeTerminalGaps = TRUE,
                                   noConsensusChar = "-")[[1]]
 
+
+    print("Calculting differences between reads and consensus")
     diffs = mclapply(aln, n.pairwise.diffs, subject = consensus, mc.cores = processors)
     diffs = do.call(rbind, diffs)
     diffs.df = data.frame("name" = names(aln), "pairwise.diffs.to.consensus" = diffs[,1], "unused.chars" = diffs[,2])
     rownames(diffs.df) = NULL
 
     # get a dendrogram
-    dist = DistanceMatrix(aln, correction = "Jukes-Cantor", penalizeGapLetterMatches = FALSE, processors = processors)
-    dend = IdClusters(dist, asDendrogram = TRUE, processors = processors)
+    dist = DistanceMatrix(aln, correction = "Jukes-Cantor", penalizeGapLetterMatches = FALSE, processors = processors, verbose = FALSE)
+    dend = IdClusters(dist, asDendrogram = TRUE, processors = processors, verbose = FALSE)
 
     # add consensus to alignment
     aln2 = c(aln, DNAStringSet(consensus))
@@ -94,7 +97,7 @@ merge.reads <- function(fwd.seqs, rev.seqs, ref.aa.seq = NULL, minInformation = 
 
     return(list("consensus" = consensus.gapfree, 
                 "alignment" = aln2, 
-                "diffs" = diffs.df, 
+                "differences" = diffs.df, 
                 "distance.matrix" = dist,
                 "dendrogram" = dend,
                 "indels" = indels)) 
