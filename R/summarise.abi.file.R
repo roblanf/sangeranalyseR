75dcf936b4b5902ba3744459bd62a4cb18a334b3
RobLanf <rob.lanfear@gmail.com>
2015-12-23 11:18:53 +1100

    update summarise.abi.file documentation and return seqs
    the documentation was outdated. Better none for now.

--- R/summarise.abi.file.R
+++ R/summarise.abi.file.R
@@ -1,15 +1,5 @@
 #' Create a detailed summary of a single ABI sequencing file
 #' 
-#' This function recursively scans a folder that contains abi files either in itself or its subfolders, and looks for secondary peaks in each sequence. It produces an annotated plot of the secondary peaks above a user-defined cutoff, as well as a table of the mismatches.
-#' 
-#' It recursively scanes the input folder for all abi files. For each abi file, it creates and returns a data frame and a plot of mismatches, and also writes both to the same file as the .ab1 file (as a .csv and .pdf respectively).
-#'
-#' @param input.folder the folder in which to search recursively for *.ab1 files.
-#' @param cutoff the ratio of the height of a secondary peak to a primary peak. Secondary peaks higher than this ratio are annotated. Those below the ratio are not. 
-#' @param write.files TRUE/FALSE. FALSE (the default): do not write any files to disk. TRUE: write plots and .csv files of secondary peaks to disk, in this case, files are named with the same filename as the .ab1 files, but with .csv and .pdf extensions, and are saved in the same folder as the .ab1 file from which they were created.
-#' @param processors The number of processors to use, or NULL (the default) for all available processors
-#' 
-#' @return output a dataframe of secondary peaks
 #'
 #' @keywords chromatogram, peak, mismatch
 #'
@@ -43,14 +33,16 @@ summarise.abi.file <- function(inputfile, trim.cutoff = 0.0001, secondary.peak.c
     qual.trimmed = qual[trim.start:trim.finish]
     secondary.peaks.trimmed = subset(secondary.peaks, position > trim.start && position < trim.end )
 
-    return(c("raw length"               = length(seq@primarySeq), 
-             "trimmed length"           = length(seq.trimmed), 
-             "raw secondary peaks"      = nrow(secondary.peaks),
-             "trimmed secondary peaks"  = nrow(secondary.peaks.trimmed),
-             "raw mean quality"         = mean(qual),
-             "trimmed mean quality"     = mean(qual.trimmed),
-             "raw min quality"          = min(qual),
-             "trimmed min quality"      = min(qual.trimmed)
+    return(c("seq"                      = seq,
+             "seq.trimmed"              = seq.trimmed,
+             "raw.length"               = length(seq@primarySeq), 
+             "trimmed.length"           = length(seq.trimmed), 
+             "raw.secondary.peaks"      = nrow(secondary.peaks),
+             "trimmed.secondary.peaks"  = nrow(secondary.peaks.trimmed),
+             "raw.mean.quality"         = mean(qual),
+             "trimmed.mean.quality"     = mean(qual.trimmed),
+             "raw.min.quality"          = min(qual),
+             "trimmed.min.quality"      = min(qual.trimmed)
              ))
 
 }
