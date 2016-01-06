360968870ccad6bf2240fe470b0688fa7a410413
RobLanf <rob.lanfear@gmail.com>
2016-01-07 10:57:57 +1100

    shell function to summarise merged reads
    there’s a lot of information in the output, and we need a simple
    summary to make a nice data frame of merged reads.
    
    E.g.
    
    1. Length
    2. Sequence
    3. # reads
    4. mean pairwise distance between reads
    5. median read length
    6. min read length
    7. max read length
    8. min, median, max secondary peaks
    9. all other min median max summaries of read.summary df
    10. # stops in frame 1, 2, 3
    11. Indeed summary (NA if NA)

---
+++ R/summarise.merged.read.R
@@ -0,0 +1,14 @@
+#' Produce a one-row summary of a merged read
+#' 
+#' @param merged.read a merged.read object produced by the merge.reads() function
+#'
+#' @return A one-row summary of the merged read
+#'
+#' @export make.readset
+
+
+summarise.merged.read <- function(merged.read){
+
+    
+
+}
