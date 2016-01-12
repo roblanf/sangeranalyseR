0ccb6c4cf565644fc90d416ed8b901a28ee180f7
RobLanf <rob.lanfear@gmail.com>
2016-01-13 09:58:55 +1100

    document merged.read

--- R/summarise.merged.read.R
+++ R/summarise.merged.read.R
@@ -2,12 +2,33 @@
 #' 
 #' @param merged.read a merged.read object produced by the merge.reads() function
 #'
-#' @return A one-row summary of the merged read
+#' @return A numeric vector including (note that insertions, deletions, and stop codons will only be called if the read was merged with a reference amino acid sequence):
+#'          \enumerate{
+#'              \item {consensus.len}: the length of the consensus sequence\cr
+#'              \item {n.reads}: the number of reads used to build the consensus\cr
+#'              \item {distance.min}: the minimum distance between two reads \cr
+#'              \item {distance.max}: the maximum distance between two reads \cr
+#'              \item {distance.med}: the median distance between pairs of reads \cr
+#'              \item {readlen.min}: the length of the smallest read \cr
+#'              \item {readlen.max}: the length of the longest read \cr
+#'              \item {readlen.med}: the median of all read lengths \cr
+#'              \item {insertions.min}: the smallest number of insertions inferred in a read \cr
+#'              \item {insertions.max}: the larges number of insertions inferred in a read \cr
+#'              \item {insertions.med}: the median number of insertions inferred in a read \cr
+#'              \item {deletions.min}: the smallest number of deletions inferred in a read \cr
+#'              \item {deletions.max}: the larges number of deletions inferred in a read \cr
+#'              \item {deletions.med}: the median number of deletions inferred in a read \cr
+#'              \item {stops.min}: the smallest number of stop codons inferred in a read \cr
+#'              \item {stops.max}: the larges number of stop codons inferred in a read \cr
+#'              \item {stops.med}: the median number of stop codons inferred in a read \cr
+#'          }  
 #'
 #' @export summarise.merged.read
 
 
 summarise.merged.read <- function(merged.read){
+
+    if(class(merged.read != 'merged.read')){ stop("merged.read must be a merged.read object")}
 
     m = merged.read
     reads = m$alignment[1:(length(m$alignment)-1)]
