bd9bd3841f7fc6be5edbf0de5d52ad75282baa2e
RobLanf <rob.lanfear@gmail.com>
2016-01-12 13:08:06 +1100

    potential bug fix for processors on windows
    there’s some discussion online about whether to use Sys.info or
    Platform. But Platform looks deprecated, so I’ll go with Sys.info for
    now.

--- R/util.R
+++ R/util.R
@@ -2,6 +2,14 @@
 
 get.processors <- function(processors){
     
+    if(Sys.info()["sysname"] == 'Windows'){
+        # mclapply is not supported on windows
+        # so we give a single processor,
+        # in which case mclapply calls fall back
+        # on lapply
+        return(1)
+    }
+
     if(is.null(processors)){ 
         processors = detectCores(all.tests = FALSE, logical = FALSE)
     }
