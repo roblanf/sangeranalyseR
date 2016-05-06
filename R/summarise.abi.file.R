9199acc3d84cd427510b56b9b096bee6370b3382
RobLanf <rob.lanfear@gmail.com>
2016-05-06 11:41:50 +1000

    fix bug in summarising quality of trimmed read

--- R/summarise.abi.file.R
+++ R/summarise.abi.file.R
@@ -40,7 +40,8 @@ summarise.abi.file <- function(seq.abif, trim.cutoff = 0.0001, secondary.peak.ra
     trims = trim.mott(seq.abif, cutoff = trim.cutoff)
     qual = seq.abif@data$PCON.2
     qual.trimmed = qual[trims$start:trims$finish]
-    if(length(qual.trimmed==0)){qual.trimmed = c(NA)} # so we can summarise later
+
+    if(length(qual.trimmed)==0){qual.trimmed = c(NA)} # so we can summarise later
 
     # now we fix up the trim locations to correspond to the sangerseq primary seq object
     if(trims$start==1 && trims$finish==nchar(as.character(seq.abif@data$PBAS.2))){
@@ -58,6 +59,7 @@ summarise.abi.file <- function(seq.abif, trim.cutoff = 0.0001, secondary.peak.ra
     seq.trimmed = seq.sanger@primarySeq[trim.start:trim.finish]
     secondary.peaks.trimmed = subset(secondary.peaks, position >= trim.start & position <= trim.finish)
 
+    print(qual.trimmed)
     read.summary = c("raw.length"                       = length(seq.sanger@primarySeq), 
                      "trimmed.length"                   = length(seq.trimmed),
                      "trim.start"                       = trim.start,
