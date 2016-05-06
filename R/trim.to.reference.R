33e4fe08100d6bd5155904e968e26b8d6183498a
RobLanf <rob.lanfear@gmail.com>
2016-05-06 18:37:29 +1000

    minor change to trim.to.reference plan

--- R/trim.to.reference.R
+++ R/trim.to.reference.R
@@ -1,10 +1,15 @@
-#' Trim an XStringSet object to the limits of a supplied reference sequence
+#' Trim an aligned XStringSet object to the limits of a supplied reference sequence
 #'
-#' This function aligns all of the sequences in the XStringSet object along with
-#' the reference sequence. It them trims the alignment to the limits of the 
+#' This function profile aligns the XStringSet object to the
+#' reference sequence. It then trims the alignment to the limits of the 
 #' reference sequence, and returns a trimmed alignment
 #'
 #' @param stringset an XStringSet object
 #' @param reference an XString object of the same type as the XStringSet object
 #'
 #' @export trim.to.reference
+
+trim.to.reference <- function(stringset, reference){
+
+
+}
