2eabf5ad2716f9e74f57cc07d667097655d3ab53
Dan Warren <dan.l.warren@gmail.com>
2017-05-16 14:35:12 +0900

    added imports to util script file

--- R/util.R
+++ R/util.R
@@ -1,7 +1,18 @@
+#' @import ape
+#' @import Biostrings
+#' @import DECIPHER
+#' @import parallel
+#' @import reshape2
+#' @import phangorn
+#' @import sangerseqR
+#' @import stringi
+#' @import stringr
+#' @importFrom stats median
+#' @importFrom utils write.csv
 
 
 get.processors <- function(processors){
-    
+
     if(Sys.info()["sysname"] == 'Windows'){
         # mclapply is not supported on windows
         # so we give a single processor,
@@ -10,7 +21,7 @@ get.processors <- function(processors){
         return(1)
     }
 
-    if(is.null(processors)){ 
+    if(is.null(processors)){
         processors = detectCores(all.tests = FALSE, logical = FALSE)
     }
 
