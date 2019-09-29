8d8f98ada5fcb2f34e8aebf703d143642d228de5
HowardChao <ntueeb05howard@gmail.com>
2019-09-29 19:37:09 +1000

    Start 'SangerReads' S4 class

---
+++ R/AllClasses.R
@@ -0,0 +1,73 @@
+#' @title SangerReads
+#'
+#' @description  An S4 class for storing reads of sanger sequencing.
+#'
+#' @slot forward.read .
+#' @slot reverse.read .
+#' @slot consensus.read .
+#' @slot quality.matrix .
+#'
+#' @name SangerReads-class
+#'
+#' @rdname SangerReads-class
+#'
+#' @exportClass SangerReads
+#' @author Kuan-Hao Chao
+#' @examples
+#' inputFilesPath <- system.file("extdata/", package = "sangeranalyseR")
+#' A_chloroticaForwardReadFN <- file.path(inputFilesPath,
+#'                                        "Allolobophora_chlorotica",
+#'                                        "ACHLO006-09[LCO1490_t1,HCO2198_t1]_F.ab1")
+#' A_chloroticaReverseReadFN <- file.path(inputFilesPath,
+#'                                        "Allolobophora_chlorotica",
+#'                                        "ACHLO006-09[LCO1490_t1,HCO2198_t1]_R.ab1")
+#' A_chloroticaRead <- new("SangerReads",
+#'                          forwardReadFileName = A_chloroticaForwardReadFN,
+#'                          reverseReadFileName = A_chloroticaReverseReadFN)
+setClass("SangerReads",
+         ### -------------------------------------------------------------------
+         ### Input type of each variable
+         ### -------------------------------------------------------------------
+         representation(
+             forwardReadFileName = "character",
+             reverseReadFileName = "character",
+             forwardRead         = "character",
+             reverseRead         = "character",
+             consensusRead       = "character",
+             qualityMatrix       = "data.frame"
+         ),
+         # ### -------------------------------------------------------------------
+         # ### Initial value
+         # ### -------------------------------------------------------------------
+         # prototype(
+         #     forwardReadFileName    = NULL,
+         #     reverseReadFileName    = NULL,
+         #     forwardRead            = NULL,
+         #     reverseRead            = NULL,
+         #     consensusRead          = "",
+         #     qualityMatrix          = data.frame()),
+         ### -------------------------------------------------------------------
+         ### Input validity check
+         ### -------------------------------------------------------------------
+         validity = function(object) {
+             errors <- character()
+             file.exists(object@forwardReadFileName)
+
+             if (!file.exists(object@forwardReadFileName)) {
+                 msg <- paste("'", object@forwardReadFileName, "'",
+                              " foward read file does not exist.", sep = "")
+                 errors <- c(errors, msg)
+             }
+             if (!file.exists(object@reverseReadFileName)) {
+                 msg <- paste("'", object@reverseReadFileName, "'",
+                              " reverse read file does not exist.", sep = "")
+                 errors <- c(errors, msg)
+             }
+         }
+)
+
+
+
+
+
+
